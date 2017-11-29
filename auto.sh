dd236c8af06308ad59a59ca3787dee43ef8f2362
wujiaming <1114802868@qq.com>
2017-11-30 07:43:55 +0800

    优化shell脚本

--- auto.sh
+++ auto.sh
@@ -5,13 +5,13 @@ dir=`basename $dir_full`
 commit=$1
 passwd=$2
 if [ $# -lt 2 ];then
-    echo "Usage:$0 commit passwd [wait_time] [upload_time]"
+    echo "Usage:$0 commit passwd [commit_time] [upload_time]"
     exit
 fi
 if [ -n "$3" ];then
-    time=$3
+    commit_time=$3
 else
-    time=4
+    commit_time=4
     echo "如果此脚本提示 Everything up-to-date，那可能是给git commit的时间不够，请尝试添加第三个参数。"
 fi
 
@@ -27,7 +27,7 @@ set timeout $upload_time
 spawn git add .
 sleep 5
 spawn git commit -m"$commit"
-sleep $time
+sleep $commit_time
 spawn git push https://github.com/wujiaming123/${dir}.git master
 expect "'https://github.com':"
 send "wujiaming123\n"
